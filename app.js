e50e5f376a5089f30ec6452ccc20ec69e0d72ddd
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2023-05-27 15:17:30 +0530

    delete user details from database

--- app.js
+++ app.js
@@ -20,12 +20,11 @@ const sequelize = require("./util/database");
 
 const User = require("./models/usermodel");
 
+// user details storing in database
 app.post("/postuserdetails", (req, res, next) => {
   const username = req.body.name;
   const mail = req.body.email;
   const phonenumber = req.body.phonenumber;
-
-  console.log(username, mail, phonenumber);
 
   const data = User.create({
     name: username,
@@ -34,6 +33,68 @@ app.post("/postuserdetails", (req, res, next) => {
   });
   res.status(200).json({ newUserDetails: data });
 });
+
+
+// getting the user details from database
+app.get("/userdetails", async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    // console.log("All users:", JSON.stringify(users, null, 2));
+    res.status(200).json({ UserDetails: users });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to retrieve user details from the database" });
+  }
+});
+
+// app.delete("/userdelete/:userId", async(req, res,next)  =>{
+//   const delUserId = req.params.userId;
+//   console.log("the delete user Id was",delUserId);
+//   try {
+//      const user = await User.findByPk(delUserId)
+//     if (user){
+//       user.destroy();
+//       res.status(200).json({message: 'User deleted successfully'});
+//     }
+//     else{
+//       res.status(404).json({message: 'User not found'});
+//     }
+     
+    
+// }
+//   catch (err) {
+//     console.log(err);
+//     res.status(500).json({ error: "Failed to delete user details from the database" });
+
+//   }
+// });
+
+
+app.delete("/userdelete/:id", async (req, res, next) => {
+  const delUserId = req.params.id;
+  console.log("The deleted user ID is", delUserId);
+  try {
+    const user = await User.findByPk(delUserId);
+    if (user) {
+      await user.destroy();
+      res.status(200).json({ message: "User deleted successfully" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to delete user details from the database" });
+  }
+});
+
+
+
+
+
+
+
+
+
 
 sequelize
   .sync()
--- public/script.js
+++ public/script.js
@@ -19,66 +19,66 @@ function saveToServer(event) {
     });
 }
 
-// //refresh window  - show user details on screen
-// window.addEventListener("DOMContentLoaded", () => {
-//   axios
-//     .get("https://crudcrud.com/api/da7471657e27485d867224b8ca7e0afb/details/")
-//     .then((response) => {
-//       for (let i = 0; i < response.data.length; i++) {
-//         displayUserDetails(response.data[i]);
-//       }
-//       //   console.log(response.data);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       document.body.innerHTML =
-//         document.body.innerHTML +
-//         "<h2>Something went wrong from getting user details from server</h2>";
-//     });
-// });
+//refresh window  - show user details on screen
+window.addEventListener("DOMContentLoaded", () => {
+  axios
+    .get("http://localhost:3000/userdetails")
+    .then((response) => {
+      const userDetails = response.data.UserDetails;
+      for (let i = 1; i < userDetails.length; i++) {
+        displayUserDetails(userDetails[i]);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      document.body.innerHTML =
+        document.body.innerHTML +
+        "<h2>Something went wrong from getting user details from server</h2>";
+    });
+});
 
-// function displayUserDetails(user) {
-//   console.log(user.name);
-//   console.log(user.email);
-//   console.log(user.phonenumber);
-//   console.log(user._id);
+function displayUserDetails(user) {
+  // console.log(user.name);
+  // console.log(user.mail);
+  // console.log(user.phonenumber);
+  console.log("the id is ",user.id);
 
-//   const parentNode = document.getElementById("userDetails");
-//   const childHTML = `<li id=${user._id}> ${user.name} - ${user.email} - ${user.phonenumber}
-//                             <button onclick=deleteUser('${user._id}')> Delete User </button>
-//                             <button onclick=editUserDetails('${user._id}','${user.name}','${user.email}','${user.phonenumber}')>Edit User </button>
-//                          </li>`;
+  const parentNode = document.getElementById("userDetails");
+  const childHTML = `<li id=${user.id}> ${user.name} - ${user.mail} - ${user.phonenumber}
+                            <button onclick = deleteUser('${user.id}')> Delete User </button>
+                            <button onclick = editUserDetails('${user.id}','${user.name}','${user.mail}','${user.phonenumber}')>Edit User </button>
+                         </li>`;
 
-//   parentNode.innerHTML = parentNode.innerHTML + childHTML;
-// }
+  parentNode.innerHTML = parentNode.innerHTML + childHTML;
+}
 
-// // Delete user details in server-side
-// function deleteUser(userId) {
-//   axios
-//     .delete(
-//       `https://crudcrud.com/api/da7471657e27485d867224b8ca7e0afb/details/${userId}`
-//     )
-//     .then((response) => {
-//       console.log(response);
-//       deleteUserInScreen(userId);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// }
 
-// // Delete user details on screen
-// function deleteUserInScreen(userId) {
-//   parentNode = document.getElementById("userDetails");
-//   childNode = document.getElementById(`${userId}`);
-//   parentNode.removeChild(childNode);
-//   alert("User deleted");
-// }
+function deleteUser(userId) {
+  axios
+    .delete(`http://localhost:3000/userdelete/${userId}`)
+    .then((response) => {
+      console.log(response);
+      deleteUserInScreen(userId);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+
+
+// Delete user details on screen
+function deleteUserInScreen(userId) {
+  parentNode = document.getElementById("userDetails");
+  childNode = document.getElementById(`${userId}`);
+  parentNode.removeChild(childNode);
+  alert("User deleted");
+}
 
 // // Edit user details
-// function editUserDetails(userId, userName, userEmail, userPhoneNumber) {
-//   document.getElementById("name").value = userName;
-//   document.getElementById("email").value = userEmail;
-//   document.getElementById("phone").value = userPhoneNumber;
-//   deleteUserInScreen(userId);
-// }
+function editUserDetails(userId, userName, userEmail, userPhoneNumber) {
+  document.getElementById("name").value = userName;
+  document.getElementById("email").value = userEmail;
+  document.getElementById("phone").value = userPhoneNumber;
+  deleteUserInScreen(userId);
+}
